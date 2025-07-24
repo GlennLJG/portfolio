5502dd7fccc755ced74d374536067b5bf4e487dc
N4rcis <glenn.groebli@gmail.com>
2025-07-24 23:38:26 +0200

    test6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
 
+console.log(' Config loaded with base:', '/portfolio')
+
 export default defineConfig({
   site: 'https://glennljg.github.io',
   base: '/portfolio',
