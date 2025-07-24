e3d012f0115281e6e7864ca5df6de7a521573ac3
N4rcis <glenn.groebli@gmail.com>
2025-07-24 23:20:11 +0200

    test en remplacant astroyml par deployyml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://glennljg.github.io/portfolio',
+  site: 'https://glennljg.github.io',
+  base: '/portfolio',
 })
